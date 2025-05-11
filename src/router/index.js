45821f4e9abf74d929b426ba0c9c398b17007efe
DESKTOP-7GB1329\13718 <137182963@qq.com>
2025-05-12 00:03:57 +0800

    build

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import WaterfallDemo from "@/views/WaterfallDemo.vue";
-let base = "/metaApp";
+let base = "/metaApp1";
 
 const routes = [
   {
